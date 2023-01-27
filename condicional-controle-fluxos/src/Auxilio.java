4f180bb459d61920bd921cf69cb766aba29cb17b
andradeju <julliana.andrade08@gmail.com>
2023-01-27 18:11:50 -0300

    lógica condicional controle de fluxos

---
+++ condicional-controle-fluxos/src/Auxilio.java
@@ -0,0 +1,19 @@
+public class Auxilio {
+    public static void main(String[] args) {
+        double salarioMensal = 11893.58d;
+        double mediaSalario = 10500d;
+
+        int quantidadeDependentes = 4;
+        int mediaDependentes = 2;
+
+        System.out.println((salarioMensal < mediaSalario) && (quantidadeDependentes >= mediaDependentes));
+
+        boolean salarioBaixo = salarioMensal < mediaSalario;
+        boolean muitosDependentes =     quantidadeDependentes >= mediaDependentes;
+
+        System.out.println((salarioBaixo) && (muitosDependentes));
+
+        boolean recebeAuxilio = (salarioBaixo) && (muitosDependentes);
+        System.out.println("recebeAuxilio " + recebeAuxilio);
+    }
+}
